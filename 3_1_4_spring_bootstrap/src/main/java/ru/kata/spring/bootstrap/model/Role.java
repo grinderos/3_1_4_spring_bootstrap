7eac9fd4330a4fc29f2b71daf343bf3160b9af61
grinderos <grinderos.coding@gmail.com>
2024-04-24 12:17:47 +0300

    Initial commit

---
+++ 3_1_4_spring_bootstrap/src/main/java/ru/kata/spring/bootstrap/model/Role.java
@@ -0,0 +1,58 @@
+package ru.kata.spring.bootstrap.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(int id) {
+        this.id = id;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+        return name.equals(role.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
---
+++ 3_1_4_spring_bootstrap/src/main/java/ru/kata/spring/bootstrap/model/User.java
@@ -0,0 +1,90 @@
+package ru.kata.spring.bootstrap.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Поле не должно быть пустым")
+    @Size(min = 3, max = 40, message = "Логин должен содержать не менее трех символов")
+    @Column(name = "username", nullable = false, unique = true, length = 32)
+    private String username;
+
+    @Column(name = "first_name", nullable = false, unique = true, length = 16)
+    private String first_name;
+
+    @Column(name = "last_name", nullable = false, unique = true, length = 16)
+    private String last_name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 32)
+    private String email;
+
+    @Column(name = "age")
+    @Min(value = 0, message = "Неверно указан возраст")
+    private Integer age;
+
+    @NotEmpty(message = "Поле не должно быть пустым")
+    @Column(name = "login_password", nullable = false, length = 64)
+    @Size(min = 4, max = 100, message = "Пароль должен содержать не менее трех символов")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE) //@Fetch(FetchMode.JOIN)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String first_name, String last_name,
+                String email, Integer age, String password) {
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
